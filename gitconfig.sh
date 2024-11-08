de2b67cd1d9021d8af4854cddf82f14809ec877f
Gaurav Chauhan <182621594+1BH23CS024@users.noreply.github.com>
2024-11-08 05:32:23 +0530

    Add gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config user.name "Gaurav Chauhan"
+git config user.email "182621594+1BH23CS024@users.noreply.github.com"
+
+git config --unset gpg.format
+git config user.signingkey "EA67D78E92DD0C37"
+git config commit.gpgsign true
